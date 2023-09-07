b207dee3f514c43d12a2f9f5da9cdf9918f082ec
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-09-07 15:25:44 +0200

    🎨 JSON powered new version!

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import './style.css'
-import {choice} from './choice.js'
+import {run} from './choice.js'
 
+run(document.querySelector('#app'));
 
-document.querySelector('#app').innerHTML = `
+/*document.querySelector('#app').innerHTML = `
           Name: Seth
           Occupation: Healer
           Address: Crystal Depths, Aqualoria
@@ -10,8 +11,6 @@ document.querySelector('#app').innerHTML = `
           Email: seth.waterhealer@aqualoria.com
 
           This is a site for my cv so people know who I am
-
-          I am Seth, a dedicated and compassionate water slime Healer with a profound connection to the element of water. In the enchanting realm of Aqualoria, I have honed my unique magical abilities to bring healing and solace to those in need. With a deep understanding of aquatic magic and a commitment to the well-being of all, I have established myself as a trusted and skilled healer in our mystical world.
           
 
           <button id="education">Education</button>
@@ -49,4 +48,4 @@ document.querySelector('#app').innerHTML = `
           <p id="about"></p>
           `
 
-choice(document.querySelector('#app'))
+choice(document.querySelector('#app')) */
